febbb09f8f3b5e91652d41edcd97a2f48a6a326b
bursaar <ben@ben.ie>
2014-01-13 18:17:16 +0000

    Created Windows.cpp
    Writing the window class in the CPP file.

---
+++ Maze_Runner_Blitter/Window.cpp
@@ -0,0 +1,25 @@
+#include "Window.h"
+
+namespace Train2Game
+{
+	Window::Window(HINSTANCE pInstance, WNDPROC pHdlr, const std::wstring pCaption, int pX, int pY, int pWidth, int pHeight)
+	{
+		mWindowClass.style = 0;
+		mWindowClass.cbClsExtra = 0;
+		mWindowClass.cbWndExtra = 0;
+		mWindowClass.hIcon = 0;
+		mWindowClass.hbrBackground = 0;
+		mWindowClass.lpszMenuName = 0;
+		mWindowClass.hCursor = LoadCursor(0, IDC_ARROW);
+		mWindowClass.lpfnWndProc = pHdlr;
+		mWindowClass.hInstance = pInstance;
+		mWindowClass.lpszClassName = pCaption.c_str();
+
+		ATOM success = RegisterClass(&mWindowClass);
+		if (!success)
+		{
+			// 20:26
+		}
+	}
+	
+}
