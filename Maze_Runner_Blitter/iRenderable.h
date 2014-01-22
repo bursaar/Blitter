fd284b61206075dd879f6a17ef6336244b83ae81
bursaar <ben@ben.ie>
2014-01-22 11:41:21 +0000

    Fleshing out iRenderable interface

--- Maze_Runner_Blitter/iRenderable.h
+++ Maze_Runner_Blitter/iRenderable.h
@@ -25,11 +25,32 @@ class IRenderable
 			}
 			void Release()
 			{
+				if (mVertexBuffer != nullptr)
+				{
+					mVertexBuffer->Release();
+					mVertexBuffer = nullptr;
+				}
 
+				if (mTexture != nullptr)
+				{
+					mRenderer->ReleaseTexture(mTextureName.c_str());
+					mTexture = nullptr;
+				}
+			}
+
+			void Resume()
+			{
+				mVertexBuffer = mRenderer->CreateVertexBuffer(vertices);
+				if (!mTextureName.empty())
+				{
+					mTexture = mRenderer->LoadTexture(mTextureName.c_str());
+				}
 			}
 			virtual void Initialise(Train2Game::Renderer * pRenderer)
 			{
 				mRenderer = pRenderer;
+				mVertexBuffer = pRender->CreateVertexBuffer(vertices);
+				mIsInitialised = true;
 			}
 
 	private:
