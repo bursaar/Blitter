c73bf9ba11f96658279c7966d0e6b172d154a20c
bursaar <ben@ben.ie>
2014-01-28 19:57:25 +0000

    A (sort of) working version
    I’ve fixed the path and made a small change to the ParticleEmitter.cpp
    file, changing mRenderer to pRender.
    
    Current bugs: Textures appear to be loading in code, but don’t yet
    render to screen.

--- Maze_Runner_Blitter/ParticleEmitter.cpp
+++ Maze_Runner_Blitter/ParticleEmitter.cpp
@@ -9,13 +9,13 @@ namespace Train2Game
 		vertices.push_back(Vertex(-1.0f, -1.0f, -1.0f, 0xffffffff, 0.0f, 1.0f));
 		vertices.push_back(Vertex(1.0f, -1.0f, -1.0f, 0xffffffff, 1.0f, 1.0f));
 
-		IRenderable::Initialise(mRenderer);
+		IRenderable::Initialise(pRender);
 
 		mActive = true;
 
 		mScale.x = 2.1f;
 		mScale.y = 2.1f;
 
-		SetTexture(TEXT("..\..\SpritesExported\MR_BG_White.png"));
+		SetTexture(TEXT("Debug/bin/Debug/Sprites/MR_BG_White.png"));
 	}
 }
