46bd7b112492a2c1cbad86b3ac1c703d2ca70988
bursaar <ben@ben.ie>
2014-02-11 15:11:19 +0000

    Turning data serialisation into a class of its own.

--- Maze_Runner_Blitter/Main.cpp
+++ Maze_Runner_Blitter/Main.cpp
@@ -1,15 +1,6 @@
 #include <Windows.h>
 #include "Window.h"
 #include "Renderer.h"
-#include <Writing.h>
-#include <iostream>
-#include <fstream>
-#include "ParticleEmitter.h"
-#include "Border.h"
-#include "Title.h"
-
-using namespace std;
-
 
 LRESULT CALLBACK messageHandler(HWND pHWND, UINT pMsg, WPARAM pWparam, LPARAM pLparam)
 {
@@ -25,49 +16,9 @@ LRESULT CALLBACK messageHandler(HWND pHWND, UINT pMsg, WPARAM pWparam, LPARAM pL
 
 int WINAPI WinMain(HINSTANCE pInstance, HINSTANCE pPrevInstance, PSTR pLpCmdLine, INT pCmdShow)
 {
-	/*
-	FILE * fTest = fopen("Levels/Level_1_nl", "r");
-	int iIndex = 0;
-	char chMaxBuf[338];
-	bool blReadData[338];
-	while (iIndex == 1)
-	{
-		fgets(chMaxBuf, 338, fTest);
-		if (chMaxBuf[iIndex] == '0')
-		{
-			blReadData[iIndex] = 0;
-		}
-		else 
-		{
-			blReadData[iIndex] = 1;
-		}
-		iIndex ++ ;
-	}
-
-	iIndex = 0;
-
-	while (iIndex == 0)
-	{
-		cout << blReadData[iIndex];
-		iIndex++;
-	}
-	cout << endl;
-	*/
-
-	Train2Game::Window * window = new Train2Game::Window(pInstance, messageHandler, TEXT("Train2Game Portfolio 1 Project 2 ~ Ben Keenan CD9000002O"), 150, 100, 640, 480);
+	Train2Game::Window * window = new Train2Game::Window(pInstance, messageHandler, TEXT("Train2Game Portfolio 1 Project 2 ~ Ben Keenan CD9000002O"), 50, 80, 800, 640);
 	Train2Game::Renderer * renderer = new Train2Game::Renderer(window);
-
-	// Create background objects.
-	Train2Game::cBackground * bg = new Train2Game::cBackground();
-	Train2Game::cBorder * br = new Train2Game::cBorder();
-	Train2Game::cTitle * tt = new Train2Game::cTitle();
-
 	window->Show();
-
-	// Initialise background objects.
-	bg->Initialise(renderer);
-	br->Initialise(renderer);
-	tt->Initialise(renderer);
 
 	MSG msg;
 	ZeroMemory(&msg, sizeof(msg));
@@ -80,12 +31,8 @@ int WINAPI WinMain(HINSTANCE pInstance, HINSTANCE pPrevInstance, PSTR pLpCmdLine
 		}
 
 		renderer->BeginFrame();
-		
-		// Render background objects
-		bg->Render();
-		br->Render();
-		tt->Render();
 
+		// draw all the things
 
 		if (!renderer->EndFrame())
 		{
@@ -104,15 +51,8 @@ int WINAPI WinMain(HINSTANCE pInstance, HINSTANCE pPrevInstance, PSTR pLpCmdLine
 			}
 
 			renderer->Initialise();
-			bg->Resume();
-			br->Resume();
-			tt->Resume();
 		}
 	}
-
-	delete bg;
-	delete br;
-	delete tt;
 
 	delete renderer;
 	delete window;
